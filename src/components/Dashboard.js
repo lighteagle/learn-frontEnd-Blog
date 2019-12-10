d54458fa021da67bc63b8151a465e91cb171ab61
lighteagle <sofyan.agus18@gmail.com>
2019-12-10 13:48:40 +0700

    update

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,17 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import jwt from "jsonwebtoken";
 
 function Dashboard() {
-  const [posts, setPosts] = useState({
+  const [posts, setPosts] = useState([]);
+
+  const [post, setPost] = useState({
     title: "",
     content: ""
   });
-  const [error, setError] = useState(0);
+
+  const [error, setError] = useState(null);
+  const [file, setFile] = useState(null);
 
   useEffect(() => {
     axios
@@ -18,8 +23,37 @@ function Dashboard() {
       .catch(error => setError(error.message));
   }, []);
 
-  const handleChange = event => {};
-  const handleSubmit = event => {};
+  const handleChange = event => {
+    const { name, value } = event.target;
+    setPost({
+      ...post,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    // token from login
+    const decoded = jwt.decode(localStorage.getItem("token"));
+
+    //create FormData
+    const fd = new FormData();
+    fd.append("imagePost", file);
+    fd.append("title", post.title);
+    fd.append("content", post.content);
+    fd.append("userId", decoded.user.id);
+
+    // add title, content, userId
+    axios
+      .post(`${process.env.REACT_APP_BACKEND_URI}/users-posts`, fd)
+      .then(result => console.log(result))
+      .catch(error => setError(error.message));
+  };
+
+  const handleFile = event => {
+    console.log(event.target.files);
+  };
 
   return (
     <div>
@@ -42,6 +76,9 @@ function Dashboard() {
           onChange={handleChange}
           value={posts.content}
         />
+
+        <input type="file" onChange={handleFile} />
+
         <button onClick={handleSubmit}> Add New Post</button>
       </form>
       <div id="posts">POST LIST</div>
